d509da9e12bd5ec16e1665d1b2625a20364707fa
pythonisraghav <86775049+pythonisraghav@users.noreply.github.com>
2021-07-13 18:32:01 +0530

    essay.py

---
+++ essay maker.py
@@ -0,0 +1,11 @@
+print('Welcome to essay maker\n')
+
+name = input('enter your name\n')
+age  =  int = input('enter Your age ' + name)
+class_ =  int= input('enter your class\n')
+gender = str = input('enter your gender\n')
+
+print(f'''My name is {name},
+I am a {gender},
+I am {age} years old,
+My class is {class_}th''')
